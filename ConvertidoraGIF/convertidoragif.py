02d6bd31e56110598d7972491a58580ebf339cf5
Anthomarti <anthonymarti2002@gmail.com>
2024-05-02 10:56:54 -0300

    Creado el gif

--- ConvertidoraGIF/convertidoragif.py
+++ ConvertidoraGIF/convertidoragif.py
@@ -7,13 +7,13 @@ video = VideoFileClip(video_path)
 # Trim the complete duration of the video
 # gif_clip = video.subclip(0, video.duration) <- este es el uso correcto, pero nuestro video no tiene "duracion" 
 
-gif_clip = video.subclip(0, 29)
+gif_clip = video.subclip(0, 25)
 
 # Define the path to save the full duration GIF
 gif_path = 'ConvertidoraGIF//videoGitIgnore.gif'
 
 # Save the trimmed clip as a GIF for the full duration
-gif_clip.write_gif(gif_path, fps=15)
+gif_clip.write_gif(gif_path, fps=10)
 
 # Output the path to the GIF file
 gif_path
