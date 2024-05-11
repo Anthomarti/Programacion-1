eff2946f2b5617dbf23500eba2f7bb8e7b480f67
anthomarti <anthonymarti2002@gmail.com>
2024-05-11 13:51:26 -0300

    Libros

--- BIblioteca/bibliotecaPhyton.py
+++ BIblioteca/bibliotecaPhyton.py
@@ -2,38 +2,10 @@ import csv
 
 ################################################################
 
-#Definimos la funcion que calcula el promedio
-def calcularPromedio(file):
-	# Leemos el archivo CSV
-	with open(file, 'r') as archivo_calificaciones:
-			lector = csv.reader(archivo_calificaciones)
-			next(lector)  # Saltamos la cabecera del archivo
-
-			suma_total = 0
-			contador = 0
-			for fila in lector:
-					suma_total += float(fila[1])  # Asumimos que la calificación está en la segunda columna
-					contador += 1 #contador = contador + 1
-
-	# Calculamos el promedio
-	promedio = suma_total / contador if contador != 0 else 0	
-	print(f"El promedio es: {promedio:.2f}")
-	return promedio
-
-################################################################
-
-#Definimos la funcion que escribe el promedio en un nuevo archivo
-def escribirPromedio(file, promedio):
-	# Escribimos el promedio de calificaciones en un nuevo archivo
-	with open(file, 'w') as archivo_promedio:
-			archivo_promedio.write(f"El promedio de las calificaciones es: {promedio}\n")
-
-################################################################
-
 # Definimos la funcion para agregar un estudiante y su promedio
-def agregarEstudiante (file, estudiante, calificacion):
-	with open(file, 'a+') as archivo_estudiante:
-			archivo_estudiante.write(f"\n{estudiante}, {calificacion}")
+def agregarLibro (file, libro, genero):
+	with open(file, 'a+') as archivo_libro:
+			archivo_libro.write(f"\n{libro}, {genero}")
 
 	# Definir la funcion que crea el archivo CSV, con las cabeceras "Nombre, Calificacion"
 
@@ -51,26 +23,18 @@ def crearArchivo(file, campos):
 if __name__ == '__main__':
 	# Se crean las variables con el nombre de los ficheros
 	file = 'libros.csv'
-	file_promedio = 'prueba.txt'
-
+	
 	# Creo el archivo csv con los Campos "Nombre, Calificacion"
-	campos = "Nombre, Calificacion"
+	campos = "Nombre, Genero"
 	crearArchivo(file, campos)
 
 	opcion = 'S' 
 	while opcion =='S':
 
-		nombre = input("Ingrese el nombre del estudiante: ")
-		calificacion = input("Calificacion del estudiante: ")
-		calificacion = float(calificacion)
+		nombrelibro = input("Ingrese el nombre del libro: ")
+		genero = input("Genero: ")
 
-		agregarEstudiante(file, nombre, calificacion)
+		agregarLibro(file, nombrelibro, genero)
 		
-		opcion = input("¿Desea agregar otro estudiante? (S/N): ")
+		opcion = input("¿Desea agregar otro libro? (S/N): ")
 		opcion = opcion.upper()
-	
-	#agregarEstudiante(file, "Ana", 4)
-	
-	promedio = calcularPromedio(file)
-	escribirPromedio(file_promedio, promedio)
-	
